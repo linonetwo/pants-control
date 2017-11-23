1a75d5efbd3998cf91af6c4e8701bf6452c151e9
lindongwu <lindongwu@memect.co>
2017-11-23 21:19:51 +0800

    feature: wait for previous startup procedure to end

--- app/reducers/startup.js
+++ app/reducers/startup.js
@@ -1,5 +1,5 @@
 // @flow
-import { call, put } from 'redux-saga/effects';
+import { put, take } from 'redux-saga/effects';
 
 import { loadConfigAction } from './config';
 import { loadCardFromFsAction } from './cards';
@@ -15,5 +15,6 @@ import type { IOEffect } from 'redux-saga/effects';
 
 export default function* StartUpSaga(): Generator<IOEffect, void, any> {
   yield put(loadConfigAction.trigger());
+  yield take(loadConfigAction.SUCCESS);
   yield put(loadCardFromFsAction.trigger());
 }
