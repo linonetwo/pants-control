a83ebceb6f71ad509333f065f997cf4743a97826
lindongwu <lindongwu@memect.co>
2017-11-23 10:05:57 +0800

    feature: wrap execution context in a async function

--- app/reducers/execute.js
+++ app/reducers/execute.js
@@ -3,7 +3,8 @@ import { createRoutine } from 'redux-saga-routines';
 import { takeLatest, call, all, put } from 'redux-saga/effects';
 
 import vm from 'vm';
-import fs from 'fs';
+import fs from 'fs-extra';
+import path from 'path';
 import { ApolloClient } from 'apollo-client';
 import gql from 'graphql-tag';
 import { HttpLink } from 'apollo-link-http';
@@ -28,14 +29,22 @@ export const executeCodeAction = createRoutine('executeCode');
 function* executeCode(action) {
   const { code, id, language }: { [x: string]: string } = action.payload;
   try {
-    let stdout: string = '';
+    let stdout: any = '';
     if (language === 'js/babel') {
       const client = new ApolloClient({
         link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
         cache: new InMemoryCache(),
       });
-      console.log(remote.app.getPath);
-      stdout = String(vm.runInNewContext(code, { client, gql, console, fs, getPath: remote.app.getPath }));
+      const asyncCode = `'use strict';
+        async function runInVM() {
+          try {
+            ${code};
+          } catch(error) {
+            console.error(error);
+          }
+        };
+        runInVM()`;
+      stdout = yield vm.runInNewContext(asyncCode, { client, gql, console, fs, path, getPath: remote.app.getPath });
     }
     console.log(stdout);
     yield put(executeCodeAction.success({ id, stdout }));
