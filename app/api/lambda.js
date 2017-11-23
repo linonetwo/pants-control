a8551d18123a500f202d43afb11ca32f0fcf5286
lindongwu <lindongwu@memect.co>
2017-11-23 13:19:14 +0800

    feature: enable lambda to load card from fs

--- app/api/lambda.js
+++ app/api/lambda.js
@@ -1,10 +1,12 @@
+// @flow
 import vm from 'vm';
-import fs from 'fs';
+import fs from 'fs-extra';
+import path from 'path';
 import { ApolloClient } from 'apollo-client';
 import gql from 'graphql-tag';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { app } from 'electron';
+import { remote } from 'electron';
 
 import { store } from '../store';
 
@@ -14,17 +16,27 @@ export default async function runLambda(req, res) {
     .getState()
     .cards.get('cards')
     .find(card => card.get('id') === noteID);
-  if (matchedCard && matchedCard.get('content').blocks) {
-    const code: string = matchedCard
-      .get('content')
-      .blocks.map(block => block.text)
-      .join('\n');
-    let stdout = '';
+  if (matchedCard && matchedCard.get('content')) {
+    let code: string = '';
+    const contentJSON = JSON.parse(matchedCard.get('content'));
+    if (contentJSON.blocks) {
+      code = contentJSON.blocks.map(block => block.text).join('\n');
+    }
+    let stdout: any = '';
     const client = new ApolloClient({
       link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
       cache: new InMemoryCache(),
     });
-    stdout = String(await vm.runInNewContext(code, { client, gql, console, fs, getPath: app.getPath }));
+    const asyncCode = `'use strict';
+    async function runInVM() {
+      try {
+        ${code};
+      } catch(error) {
+        console.error(error);
+      }
+    };
+    runInVM()`;
+    stdout = await vm.runInNewContext(asyncCode, { client, gql, console, fs, path, getPath: remote.app.getPath });
     res.send(stdout);
   } else {
     res.send(req.params);
