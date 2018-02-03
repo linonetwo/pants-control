08f6c6c8ef1f4f14dee4e17cf93290df4020e356
lindongwu <lindongwu@memect.co>
2018-02-03 17:30:31 +0800

    refactor: api now depends on ipfsd

--- src/api/index.js
+++ src/api/index.js
@@ -7,24 +7,24 @@ import schema from './schema';
 import runLambda from './lambda';
 import { initIPFS } from './ipfs';
 
-const { address } = window.__args__;
-if (address) {
-  console.log(`Init IPFS node using swarm address ${address}`);
-  const ipfs = initIPFS(address);
+const { addresses } = window.__args__;
+if (!addresses || !addresses[0]) {
+  console.error(`No swarm addresses passed from window.__args__: ${JSON.stringify(window.__args__)}`);
 } else {
-  console.error(`No swarm address passed from window.__args__: ${window.__args__}`);
+  console.log(`Init IPFS node using swarm addresses ${addresses[0]}`);
+  const ipfs = initIPFS(addresses);
+
+  const app = express();
+  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+  app.use(
+    '/graphiql',
+    graphiqlExpress({
+      endpointURL: '/graphql',
+    }),
+  );
+
+  app.all('/lambdav1/:noteID', bodyParser.json(), runLambda);
+
+  app.listen(6006);
 }
-
-const app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: '/graphql',
-  }),
-);
-
-app.all('/lambdav1/:noteID', bodyParser.json(), runLambda);
-
-app.listen(6006);
