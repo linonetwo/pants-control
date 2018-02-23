bcd225f6e2a16ab083fa5591c2b284bfe90d4d54
lindongwu <lindongwu@memect.co>
2018-02-23 21:17:33 +0800

    feature: try to add file to ipfs and view its hash

--- src/store/epics/defaultNote/index.js
+++ src/store/epics/defaultNote/index.js
@@ -2,6 +2,7 @@
 import { Observable } from 'rxjs';
 import { delay, flatMap } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
+import { Buffer } from 'buffer';
 
 import { appStart, loadNote, focusNote } from '../../actions/core';
 
@@ -17,13 +18,26 @@ export default (action, store, { ipfs }) => {
     console.error('No ipfs passed from dependency.');
     return Observable.empty();
   }
+  const fileString = JSON.stringify(defaultProfile)
+  ipfs.files.add(Buffer.from(fileString), (err, res) => {
+    if (err || !res) {
+      return console.error('ipfs add error', err, res)
+    }
+
+    res.forEach((file) => {
+      if (file && file.hash) {
+        console.log('successfully stored', file.hash)
+        display(file.hash)
+      }
+    })
+  })
   const profileHash = 'aaa';
   return action.pipe(
     ofType(appStart.TRIGGER),
     delay(1000),
     flatMap(() =>
       Observable.concat(
-        Observable.of(loadNote.success({ id: profileHash, note: JSON.stringify(defaultProfile) })),
+        Observable.of(loadNote.success({ id: profileHash, note: fileString })),
         Observable.of(focusNote(profileHash)),
       )),
   );
