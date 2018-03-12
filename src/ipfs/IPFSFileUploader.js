d195bedabb6452c7b302e4aec2204e27fc58d91f
lindongwu <lindongwu@memect.co>
2018-03-12 13:18:43 +0800

    bugfix: prevent port collision, since there is another node running in daemon in main process

--- src/ipfs/IPFSFileUploader.js
+++ src/ipfs/IPFSFileUploader.js
@@ -11,7 +11,16 @@ export default class IPFSFileUploader {
   constructor(onReady: Function = () => {}) {
     // 用随机的仓库地址（IPFS 在本地缓存数据的地方）来初始化 IPFS 节点
     const repoPath = `ipfs-${Math.random()}`;
-    this.node = new IPFS({ repo: repoPath });
+    this.node = new IPFS({
+      repo: repoPath,
+      config: {
+        Addresses: {
+          API: '/ip4/127.0.0.1/tcp/0',
+          Swarm: ['/ip4/0.0.0.0/tcp/0'],
+          Gateway: '/ip4/0.0.0.0/tcp/0',
+        },
+      },
+    });
 
     // 节点完成初始化并开始连接其他节点后会触发 ready 事件
     this.node.on('ready', () => {
