0d66ce0e7f3e0c6b80f1c417e863f8d2d8a52b61
lindongwu <lindongwu@memect.co>
2018-03-12 23:23:27 +0800

    refactor: add file to IPFS and focus on this hash

--- src/store/epics/defaultNote/index.js
+++ src/store/epics/defaultNote/index.js
@@ -1,6 +1,6 @@
 // @flow
 import { Observable } from 'rxjs';
-import { delay, flatMap } from 'rxjs/operators';
+import { delay, map, mergeMap } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 import str2arr from 'string-to-arraybuffer';
 
@@ -14,22 +14,20 @@ import defaultProfile from './defaultProfile.json';
  * load default notes, add them to ipfs, add multihash to notes store cache
  * set current note to one of default note.
  * */
-export default (action: ActionType, store: any, { ipfs }: { ipfs: IPFSFileUploader }) => action.pipe(
+export default (action: ActionType, store: any, { ipfs }: { ipfs: IPFSFileUploader }) =>
+  action.pipe(
     ofType(ipfsNodeStart.TRIGGER),
-    delay(1000),
-    flatMap(() => {
+    mergeMap(() => {
       if (!ipfs) {
         console.error('In epic defaultNote: No ipfs passed from dependency.');
         return Observable.empty();
       }
       const fileString = JSON.stringify(defaultProfile);
       const fileBuffer = str2arr(fileString);
-      console.log('aaa')
-      ipfs.uploadArrayBuffer(fileBuffer).then(console.log).catch(console.error);
-      const profileHash = 'aaa';
-      return Observable.concat(
-        Observable.of(loadNote.success({ id: profileHash, note: fileString })),
-        Observable.of(focusNote(profileHash)),
-      );
+      return Observable.fromPromise(ipfs.uploadArrayBuffer(fileBuffer)).map(result => ({ ...result, fileString }));
     }),
+    mergeMap(({ hash, fileString }) => Observable.concat(
+        Observable.of(loadNote.success({ id: hash, note: fileString })),
+        Observable.of(focusNote(hash)),
+      )),
   );
