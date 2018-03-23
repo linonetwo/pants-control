de60143313dce1da65e82bd22d41003f367091ae
lindongwu <lindongwu@memect.co>
2018-03-23 23:28:37 +0800

    bugfix: should use saulted UIntArray password as key in aes256

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,20 +1,18 @@
 /* eslint-disable new-cap */
 // @flow
-import { flow } from 'lodash';
 import aesjs from 'aes-js';
+import scrypt from 'scrypt-async-modern';
 
 const ctrCounter = 7;
 
-export const encrypt = (password: string, value: string) =>
-  flow([
-    aesjs.utils.utf8.toBytes,
-    new aesjs.ModeOfOperation.ctr(password, new aesjs.Counter(ctrCounter)).encrypt,
-    aesjs.utils.hex.fromBytes,
-  ])(value);
+export const encrypt = async (name: string, password: string, value: string): string => {
+  const key: Array<number> = await scrypt(password, name);
+  const aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(ctrCounter));
+  return aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(value)));
+};
 
-export const decrypt = (password: string, value: string) =>
-  flow([
-    aesjs.utils.hex.toBytes,
-    new aesjs.ModeOfOperation.ctr(password, new aesjs.Counter(ctrCounter)).decrypt,
-    aesjs.utils.utf8.fromBytes,
-  ])(value);
+export const decrypt = async (name: string, password: string, value: string): string => {
+  const key: Array<number> = await scrypt(password, name);
+  const aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(ctrCounter));
+  return aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(value)));
+};
