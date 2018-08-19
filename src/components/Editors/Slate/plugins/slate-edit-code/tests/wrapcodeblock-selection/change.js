197d4d422ea9cba1055b0eac0b51a0dc11d4cfcc
linonetwo <linonetwo012@gmail.com>
2018-08-19 23:57:28 +0800

    chore: delete useless tests

--- src/components/Editors/Slate/plugins/slate-edit-code/tests/wrapcodeblock-selection/change.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert';
-
-export default function(plugin, change) {
-    plugin.changes.wrapCodeBlock(change);
-
-    assert.equal(change.selection.start.offset, 5);
-
-    return change;
-}
